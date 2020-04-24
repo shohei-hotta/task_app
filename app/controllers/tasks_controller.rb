8790ecb2618f374f5d4c173c43e65aff2c82156a
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-24 07:27:55 +0000

    タスクの編集機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,6 +22,17 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を編集しました。"
+    else
+      flash.now[:alert] = "タスクを編集できませんでした。"
+      render :edit
+    end
   end
 
   private
