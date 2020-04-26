6558ec2899a8267c4ca9764602c76088e97961a7
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-26 12:15:33 +0000

    Taskモデルに関するバリデーションを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,8 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
+RSpec.describe "タスク管理機能", type: :model do
   it "名称が入力されていなければ無効" do
-    task = Task.new(description: "詳しい説明")
+    task = Task.new(name: nil)
+    task.valid?
+    expect(task.errors[:name]).to include("を入力してください")
+  end
+
+  it "名称が31文字以上であれば無効" do
+    task = Task.new(name: "a" * 31)
+    task.valid?
+    expect(task.errors[:name]).to include("は30文字以内で入力してください")
+  end
+
+  it "詳しい説明が入力されていなければ無効" do
+    task = Task.new(description: nil)
+    task.valid?
+    expect(task.errors[:description]).to include("を入力してください")
+  end
+
+  it "詳しい説明が201文字以上であれば無効" do
+    task = Task.new(description: "a" * 201)
+    task.valid?
+    expect(task.errors[:description]).to include("は200文字以内で入力してください")
+  end
+
+  it "名称と詳しい説明が規定の文字数で入力されていれば有効" do
+    task = Task.new(name: "テストタスク", description: "テストタスクの説明")
     expect(task).to be_valid
   end
 end
