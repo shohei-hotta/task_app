12fda83990b0ae982f9fbe3c10dc48e26fd36cd7
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-26 06:55:51 +0000

    日本語部分を共通化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,9 +15,9 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to task_url(@task.id), notice: "タスク「#{@task.name}」を登録しました。"
+      redirect_to task_url(@task.id), notice: "「#{@task.name}」#{t("view.flash.create_message")}"
     else
-      flash.now[:alert] = "タスクを登録できませんでした。"
+      flash.now[:alert] = "「#{@task.name}」#{t("view.flash.create_alert")}"
       render :new
     end
   end
@@ -27,16 +27,16 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to tasks_url, notice: "タスク「#{@task.name}」を編集しました。"
+      redirect_to tasks_url, notice: "「#{@task.name}」#{t("view.flash.edit_message")}"
     else
-      flash.now[:alert] = "タスクを編集できませんでした。"
+      flash.now[:alert] = "「#{@task.name}」#{t("view.flash.edit_alert")}"
       render :edit
     end
   end
 
   def destroy
     @task.destroy
-    redirect_to tasks_path, notice: "タスク「#{@task.name}」を削除しました。"
+    redirect_to tasks_path, notice: "「#{@task.name}」#{t("view.flash.destroy_message")}"
   end
 
   private
