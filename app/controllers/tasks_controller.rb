647fa341e7a392f9f623c9742c16398ce960770b
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-28 02:44:51 +0000

    tasksコントロールのStrong Parametersを変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :deadline)
   end
 
   def set_task
