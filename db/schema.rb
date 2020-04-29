89d0b08b33591a8a922dbab0a4bad555aa58036a
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-29 03:49:12 +0000

    tasksテーブルにstatusカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_020546) do
+ActiveRecord::Schema.define(version: 2020_04_29_034620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,8 @@ ActiveRecord::Schema.define(version: 2020_04_28_020546) do
     t.string "description", limit: 200, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "deadline", default: "2030-04-26 23:59:59", null: false
+    t.datetime "deadline", default: "9999-12-31 23:59:59", null: false
+    t.string "status", default: "未着手", null: false
   end
 
 end
