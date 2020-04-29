3913c2a5e1ca76cca1ea2a25ce9f4a62d8adb192
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-29 20:08:16 +0000

    tasksテーブルのpriorityカラムにEnumを適用

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 200 }
 
+  enum priority: { 低: 0, 中: 1, 高: 2 }
+
   scope :select_index, -> { select(:id, :name, :description, :deadline, :status, :created_at) }
   scope :search_name, ->(params) { where("name LIKE ?", "%#{params}%") }
   scope :search_status, ->(params) { where(status: params) }
