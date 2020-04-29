7fc15b05fb1b08346525824244d38bc25817741d
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-29 17:35:37 +0000

    models/task.rbにscopeを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 200 }
+
+  scope :search_name, -> { where("name LIKE ?", "%#{params[:search][:name]}%") }
+  scope :search_status, -> { where(status: params[:search][:status]) }
+  scope :search_name_and_status, -> { search_name.search_status }
+  scope :recent, -> { order(created_at: :DESC) }
+  scope :sort_deadline, -> { order(:deadline) }
 end
