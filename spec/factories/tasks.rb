3604e132c7f34675d36fcd44b6ac694fb0c6bc18
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-29 19:41:11 +0000

    タスクの検索機能のテストコードを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { "最初のタスク" }
     description { "タスクの説明" }
     deadline { "2020-04-01" }
+    status { "未着手" }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -18,7 +18,7 @@ describe "タスク管理機能", type: :system do
 
     context "複数のタスクを作成したとき" do
       before do
-        FactoryBot.create(:task, name: "次のタスク", deadline: "2020-04-30")
+        FactoryBot.create(:task, name: "次のタスク", deadline: "2020-04-30", status: "完了")
         visit tasks_path
       end
 
@@ -28,11 +28,31 @@ describe "タスク管理機能", type: :system do
         expect(task_list[1]).to have_content "最初のタスク"
       end
 
-      it "ソートボタンを押すと終了期限の昇順で表示される", :retry =>3 do
+      it "ソートボタンを押すと終了期限の昇順で表示される", :retry =>4 do
         click_link "終了期限でソートする", href: tasks_path(sort_expired: "true")
         task_list = all("tbody tr")
         expect(task_list[0]).to have_content "最初のタスク"
         expect(task_list[1]).to have_content "次のタスク"
+      end
+
+      it "検索フォームで名称検索できる" do
+        fill_in "名称を入力してください", with: "最初"
+        click_button "検索"
+        expect(page).to have_no_content "次のタスク"
+      end
+
+      it "検索フォームで進捗検索できる" do
+        select "完了", from: "search_form_status"
+        click_button "検索"
+        expect(page).to have_no_content "最初のタスク"
+      end
+
+      it "検索フォームで名称と進捗の両方で検索できる" do
+        fill_in "名称を入力してください", with: "次"
+        select "未着手", from: "search_form_status"
+        click_button "検索"
+        expect(page).to have_no_content "最初のタスク"
+        expect(page).to have_no_content "次のタスク"
       end
     end
   end
@@ -44,6 +64,7 @@ describe "タスク管理機能", type: :system do
         fill_in "名称", with: "新規タスク"
         fill_in "詳しい説明", with: "新規タスクの説明"
         fill_in "終了期限", with: "04/28/2020"
+        select "着手中", from: "進捗"
         click_button "登録する"
       end
 
@@ -51,6 +72,7 @@ describe "タスク管理機能", type: :system do
         expect(page).to have_content "新規タスク"
         expect(page).to have_content "新規タスクの説明"
         expect(page).to have_content "2020年04月28日"
+        expect(page).to have_content "着手中"
       end
     end
   end
