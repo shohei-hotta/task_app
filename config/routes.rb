eea37461852558eadce6fcf810adcfa85ce4247c
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-01 05:26:06 +0000

    ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root to: "tasks#index"
   resources :tasks
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
