e8eee44dc977d645ef7d882a5a6e2bfcc41ffddf
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-01 09:05:49 +0000

    ログイン機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
 
   add_flash_types :success, :info, :warning, :danger
+
+  include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,26 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: session_params[:email].downcase)
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to tasks_url, success: "#{t("view.flash.login_message")}"
+    else
+      flash.now[:danger] = "#{t("view.flash.login_alert")}"
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to new_session_url, success: "#{t("view.flash.logout_message")}"
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
