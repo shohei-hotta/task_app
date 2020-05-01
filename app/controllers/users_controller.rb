c98bf82b720bcb1a81f87c60d75591d619003697
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-01 03:12:22 +0000

    ユーザー登録機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user.id), success: "「#{@user.name}」#{t("view.flash.create_message")}"
+    else
+      flash:now[:danger] = "#{t("view.flash.create_alert")}"
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_digest)
   end
 end
