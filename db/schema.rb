da5bad6aff68bc1970c3789b43021e1c889c2d00
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-01 05:23:16 +0000

    TaskとUserを紐づけ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_023017) do
+ActiveRecord::Schema.define(version: 2020_05_01_050952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2020_05_01_023017) do
     t.datetime "deadline", default: "9998-12-31 23:59:59", null: false
     t.string "status", default: "未着手", null: false
     t.integer "priority", default: 0, null: false
+    t.bigint "user_id"
     t.index ["name"], name: "index_tasks_on_name"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_05_01_023017) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
