31a2e2323283d307893becfffb862da342959f05
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-02 09:51:31 +0000

    管理画面からユーザー情報を編集できる機能を追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -16,6 +18,15 @@ class Admin::UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_url, success: "「#{@user.name}」#{t("view.flash.edit_message")}"
+    else
+      flash.now[:danger] = "#{t("view.flash.edit_alert")}"
+      render :edit
+    end
+  end
+
   def index
     @users = User.select(:id, :name, :email, :created_at)
   end
@@ -24,5 +35,9 @@ class Admin::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
