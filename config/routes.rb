1bee0625131daa780f39344b819a7d7674c4ba28
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-02 08:54:40 +0000

    admin/usersコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/index'
+  end
   root to: "sessions#new"
   resources :tasks
   resources :users, only: [:new, :create, :show]
