72bc28f07903386983eafac174ea5828ed4c9210
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-02 09:22:23 +0000

    管理画面からユーザを登録する機能を追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,27 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_url, success: "「#{@user.name}」#{t("view.flash.create_message")}"
+    else
+      flash.now[:danger] = "#{t("view.flash.create_alert")}"
+      render :new
+    end
   end
 
   def edit
   end
 
   def index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
