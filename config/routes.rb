00bcfa5c85457481481054ba05de9b1cbec96dbc
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-02 08:58:30 +0000

    adminに関するルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'users/new'
-    get 'users/edit'
-    get 'users/index'
-  end
   root to: "sessions#new"
   resources :tasks
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  namespace :admin do
+    resources :users, except: [:show]
+  end
 end
