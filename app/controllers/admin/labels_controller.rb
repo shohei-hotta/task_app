7ceb0d94fe8b4d74652a4232480e04d8151ad6aa
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-05 08:19:08 +0000

    admin/labelsコントローラーを作成

---
+++ app/controllers/admin/labels_controller.rb
@@ -0,0 +1,10 @@
+class Admin::LabelsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'labels/index'
+    get 'labels/new'
+    get 'labels/edit'
+  end
   root to: "sessions#new"
   resources :tasks
   resources :users, only: [:new, :create, :show]
