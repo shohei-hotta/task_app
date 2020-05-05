a50f0b27cbd2af76fc1bea656770c3a92f9e1c7d
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-05 08:38:24 +0000

    管理画面からラベルを登録する機能を追加

--- app/controllers/admin/labels_controller.rb
+++ app/controllers/admin/labels_controller.rb
@@ -3,8 +3,25 @@ class Admin::LabelsController < ApplicationController
   end
 
   def new
+    @label = Label.new
+  end
+
+  def create
+    @label = Label.new(label_params)
+    if @label.save
+      redirect_to admin_labels_url, success: "「#{@label.name}」#{t("view.flash.create_message")}"
+    else
+      flash.now[:danger] = "#{t("view.flash.create_alert")}"
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def label_params
+    params.require(:label).permit(:name)
   end
 end
