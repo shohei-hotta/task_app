f0cb6cfedd98398f5713355b32e27c5e2b094ce6
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-05-05 08:21:36 +0000

    admin/labelsに関するルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'labels/index'
-    get 'labels/new'
-    get 'labels/edit'
-  end
   root to: "sessions#new"
   resources :tasks
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   namespace :admin do
     resources :users
+    resources :labels, except: [:show]
   end
 end
